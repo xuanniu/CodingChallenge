b184851f655cdb9b6f9d6ec7d310375f81e648ae
xuanniu <xuanniu@gmail.com>
2022-07-28 12:50:32 -0400

    Added OddishOrEvenish

---
+++ OddishOrEvenish.js
@@ -0,0 +1,16 @@
+function oddishOrEvenish(num) {
+	sum = 0
+  while (num > 0) {
+    sum += num % 10
+    num = Math.floor(num / 10)
+  }
+  
+  if (sum % 2 == 0) {
+  	return "Evenish"
+  } else {
+  	return "Oddish"
+  }
+}
+
+console.log(oddishOrEvenish(121))
+console.log(oddishOrEvenish(41))
